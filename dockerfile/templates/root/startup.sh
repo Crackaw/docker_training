583e5995b67dacc1ab1cff18fdf9b070211385bc
Maciej Krajewski <maciejkra@gmail.com>
2016-11-10 11:40:19 +0100

    Update startup.sh

--- dockerfile/templates/root/startup.sh
+++ dockerfile/templates/root/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mysqladmin -u root password "pass"
-mysql -u root -ppass -e "create database wordpress"
+mysqladmin -u root password "${MYSQL_ROOT_PASS}"
+mysql -u root -p${MYSQL_ROOT_PASS} -e "create database ${MYSQL_DB}"
